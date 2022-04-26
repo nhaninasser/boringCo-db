bff4d68d04226d0c41ca4194ecbbe2bba0586347
Nasser Nasser <nhani.nasser@gmail.com>
2022-04-26 19:27:31 -0400

    added more data into seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,8 @@
+-- Inserts data into department table
 INSERT INTO department (name)
 VALUES ('Management'), ('Engineers'), ('Interns');
 
+-- Inserts data into role table
 INSERT INTO role (title, salary, department_id)
 VALUES
 ("CEO", 500000.00, 1),
@@ -10,5 +12,15 @@ VALUES
 ("Mechanical Engineer", 160000.00, 2),
 ("Entry Level", 85000.00, 3),
 ("Intern", 55000.00, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+      ("Elon", "Musk", 1, NULL),
+      ("Steve", "Davis", 2, 1),
+      ("Seth", "Hopper", 4, 2),
+      ("Mark", "Muniz", 5, NULL),
+      ("Joey", "Diaz", 3, NULL),
+      ("Joe", "Rogan", 6, 3),
+      ("Andrew", "Shulz", 7, 3);
 
 
