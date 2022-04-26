6855df98bf5a5949dd1505dd8086e9684b40fd66
Nasser Nasser <nhani.nasser@gmail.com>
2022-04-26 18:53:17 -0400

    populating seeds table

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (name)
+VALUES ('Management'), ('Engineers'), ('Interns');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+("CEO", 500000.00, 1),
+("Vice President", 300000.00, 1),
+("Software Engineer", 100000.00. 2),
+("Electrical Engineer", 180000.00, 2),
+("Mechanical Engineer", 160000.00, 2),
+("Entry Level", 85000.00, 3),
+("Intern", 55000.00, 3);
+
+
