bb1ae47d9ab1019d441f4ffd1223da8006a06212
Nasser Nasser <nhani.nasser@gmail.com>
2022-04-29 17:36:08 -0400

    added get route for departments

---
+++ routes/apiRoutes/departmentRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.router();
+const db = require('../../db/connection');
+
+
+//gets the departments
+router.get('/departments', (req, res) => {
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
